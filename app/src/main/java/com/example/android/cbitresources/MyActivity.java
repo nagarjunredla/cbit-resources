5bcf89dad730969b98156a5f385c73021f3f9cac
nagarjunredla <nagarjunredla@gmail.com>
2014-10-29 13:04:21 +0530

    Added about activity

--- app/src/main/java/com/example/android/cbitresources/MyActivity.java
+++ app/src/main/java/com/example/android/cbitresources/MyActivity.java
@@ -1,11 +1,11 @@
 package com.example.android.cbitresources;
 
 import android.app.Activity;
-
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -17,6 +17,7 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.content.Intent;
 
 
 public class MyActivity extends Activity
@@ -141,6 +142,10 @@ public class MyActivity extends Activity
             ((MyActivity) activity).onSectionAttached(
                     getArguments().getInt(ARG_SECTION_NUMBER));
         }
+    }
+    public void about_us()
+    {
+        Intent intent = new Intent(this, aboutactivity.class);
     }
 
 }
